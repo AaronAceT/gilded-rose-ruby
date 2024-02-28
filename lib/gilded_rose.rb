c60d14d17501468974340191ad42717004bbadd3
Aaron Turner <turneraaron67@gmail.com>
2024-02-28 03:05:39 -0600

    cresating seventh commit

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,94 +1,53 @@
-class GildedRose
-  attr_reader :name, :days_remaining, :quality
+module GildedRose
+  class Item
+    attr_reader :quality, :days_remaining
+    def initialize(quality:, days_remaining:)
+      @days_remaining = days_remaining
+      @quality = quality
+    end
+    def tick 
+    end
+  end 
 
-  def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
+  class Normal Item < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0 
+
+      @quality -= 1 
+      @quality -= 1 if @days_remaining <= 0
+    end
   end
 
-  def tick
-    case name
-    when "normal"
-      return normal_item_tick
-    when "Aged Brie"
-      return brie_tick
-    when 'Sulfuras, Hand of Ragnaros'
-        return sulfuras_tick
-    when "Backstage passes to a TAFKAL80ETC concert"
-      return backstage_tick
-    end 
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50 
 
-      end
+      @quality += 1 
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
     end
-
   end
-  def normal_item_tick
-   @days_remaining -= 1
-   return if @quality == 0
 
-   @quality -= 1
-   @quality -= 1 if @days_remaining <= 0
-end
-def brie_tick
-  @days_remaining -= 1
-  return if @quality >= 50
 
-  @quality += 1
-  @quality += 1 if @days_remaining <= 0 && @quality < 50
-end
-def sulfuras_tick
+  class Backstage < Item
+    def tick
+      @days_remaining -= 1
+      return              if @quality >= 50
+      return @quality = 0 if @days_remaining < 0 
 
-end
-def backstage_tick
-  @days_remaining -= 1
-  return if  @quality >= 50
-  return @quality = 0 if @days_remaining < 0
+      @quality += 1 
+      @quality += 1 if @days_remaining < 10 
+      @quality += 1 if @days_remaining < 5
+    end
+  end
 
-  @quality += 1
-  @quality += 1 if @days_remaining < 10
-  @quality += 1 if @days_remaining < 5
-end
-
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = { 
+    'normal'                                        => Normal Item,
+    'Aged Brie'                                     => Brie,
+    'Backstage passes to a TAFKAL80ETC concert'     => Backstage}
+  def self.new(name:,quality:,days_remaining:)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
+  end
 end
